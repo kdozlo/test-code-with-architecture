d381781152a1ca1747e0c4b0b144e1ede3c3d910
kdozlo <eodud5730@naver.com>
2024-12-27 16:20:11 +0900

    Fix: 전체 테스트시 오류 발생 문제 해결
    - @Sql를 통해 데이터 미리 생성

--- src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Optional;
 
@@ -13,41 +14,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @TestPropertySource("classpath:test-application.properties")
+@Sql("/sql/user-repository-test-data.sql")
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
     @Test
-    public void UserRepository_가_제대로_연결되었다() throws Exception {
-        //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setEmail("test@test.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("test");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa");
-
-        //when
-        UserEntity result = userRepository.save(userEntity);
-
-        //then
-        assertThat(result.getId()).isNotNull();
-    }
-
-    @Test
     public void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() throws Exception {
         //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("test@test.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("test");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa");
 
         //when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.ACTIVE);
 
         //then
@@ -57,16 +34,9 @@ class UserRepositoryTest {
     @Test
     public void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() throws Exception {
         //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("test@test.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("test");
-        userEntity.setCertificationCode("aaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa");
 
         //when
-        userRepository.save(userEntity);
-        Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.ACTIVE);
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.PENDING);
 
         //then
         assertThat(result.isEmpty()).isTrue();
@@ -75,16 +45,8 @@ class UserRepositoryTest {
     @Test
     public void findByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() throws Exception {
         //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("test@test.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("test");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa");
 
         //when
-        userRepository.save(userEntity);
         Optional<UserEntity> result = userRepository.findByEmailAndStatus("test@test.com", UserStatus.ACTIVE);
 
         //then
@@ -94,16 +56,9 @@ class UserRepositoryTest {
     @Test
     public void findByEmailAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() throws Exception {
         //given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("test@test.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("test");
-        userEntity.setCertificationCode("aaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa");
 
         //when
-        userRepository.save(userEntity);
-        Optional<UserEntity> result = userRepository.findByEmailAndStatus("test@test.com", UserStatus.ACTIVE);
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("test@test.com", UserStatus.PENDING);
 
         //then
         assertThat(result.isEmpty()).isTrue();
