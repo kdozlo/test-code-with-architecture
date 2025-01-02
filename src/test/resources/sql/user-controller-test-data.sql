b2c3e38d40a564e4a32c979f3737f2109fc9d1df
kdozlo <eodud5730@naver.com>
2025-01-02 13:43:37 +0900

    Test: UserControllerTest 구현
    - 사용자는_특정_유저의_정보를_개인정보는_소거된채_전달_받을_수_있다
    - 사용자는_존재하지_않는_유저의_아이디로_api_호출할_경우_404_응답을_받는다
    - 사용자는_인증_코드로_계정을_활성화_시킬_수_있다
    - 사용자는_인증_코드가_일치하지_않을_경우_권한_없음_에러를_내어준다
    - 사용자는_내_정보를_불러올_때_개인정보인_주소도_갖고_올_수_있다
    - 사용자는_내_정보를_수정할_수_있다

---
+++ src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,5 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'test@test.com', 'test', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'test2@test.com', 'test2', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
